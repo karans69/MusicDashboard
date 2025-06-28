b05d8b9864e579b34224db44555e389ddd67d525
Karan Chouhan <asfscese@gmail.com>
2025-06-28 13:52:20 +0530

    role back

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,12 @@ const users = require("./usersRoutes");
 const { connectToServer } = require("./connect");
 const trackRoutes = require("./routes/trackRoutes");
 const adminTrackRoutes = require("./routes/adminTrackRoutes");
-const dashboardRoutes = require("./routes/dashboardRoutes");
-const path = require("path");
+
+
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 // Middleware
 app.use(cors());
@@ -18,31 +17,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(users);
-app.use("/api/users", users);         // user routes should be prefixed
-app.use("/api/tracks", trackRoutes);  // ✅ all track routes under /api/tracks
-app.use("/api/admin", adminTrackRoutes);
-app.use("/", dashboardRoutes);
+app.use(adminTrackRoutes);
 
 // Static Files (for artwork/audio previews)
 app.use("/uploads", express.static("uploads"));
 
+// Route Setup
+app.use("/api/users", users);         // user routes should be prefixed
+app.use("/api/tracks", trackRoutes);  // ✅ all track routes under /api/tracks
 
-
-function sendIndex(req, res) {
-  res.sendFile(path.join(__dirname, "admin-frontend/dist", "index.html"));
-}
-
-app.use(express.static(path.join(__dirname, "admin-frontend/dist")));
-
-app.get(['/dashboard', '/dashboard/*'], sendIndex);
-app.get(['/TrackList', '/TrackList/*'], sendIndex);
-app.get(['/createUser', '/createUser/*'], sendIndex);
-app.get(['/admin', '/admin/*'], sendIndex);
-app.get('*', sendIndex); // fallback
-
-
-
-
+const dashboardRoutes = require("./routes/dashboardRoutes");
+app.use("/", dashboardRoutes);
 
 // DB Connect & Server Start
 connectToServer()
